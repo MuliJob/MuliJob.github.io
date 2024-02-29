bf8a7e78efe4fa61e1ec38da5f8f7b0b23bbb0d1
Munyoki11 <jobmuli60@gmail.com>
2024-02-29 16:57:17 +0300

    changed html file name to index

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask, render_template
 app=Flask(__name__, template_folder="template")
 @app.route('/')
 def home_page():
-    return render_template('main.html')
+    return render_template('index.html')
